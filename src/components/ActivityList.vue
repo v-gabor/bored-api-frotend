1689a87375794af08d0d316cef64e458783ed06a
v-gabor <varadi.gabor@optimadesign.hu>
2020-05-20 09:48:33 +0200

    ActivityList component, #4

---
+++ src/components/ActivityList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>ActivityList komponens</div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class ActivityList extends Vue {
+  created() {
+    console.log("ActivityList komponens");
+  }
+}
+</script>
