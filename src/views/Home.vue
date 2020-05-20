352fab8683c0d86a77ee0251e7b1b0f2348fa81a
v-gabor <varadi.gabor@optimadesign.hu>
2020-05-20 09:47:16 +0200

    Modify App and Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container fluid fill-height>
+    <v-row>
+      <v-col cols="12">
+        <v-row align="center" justify="center">
+          <v-col cols="8" style="min-height: 300px; max-height: 350px;">
+            <v-tabs v-model="tab" background-color="primary" dark>
+              <v-tab v-for="item in items" :key="item.tab" :to="item.path">{{
+                item.tab
+              }}</v-tab>
+            </v-tabs>
+            <router-view class="router-view"></router-view>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+@Component
+export default class Home extends Vue {
+  tab = null;
+  items = [
+    { tab: "Activities", path: "/i-am-boring" },
+    { tab: "My list", path: "/my-activities" },
+  ];
+}
 </script>
+
+<style lang="scss">
+.router-view {
+  border: 2px solid #1976d2;
+  border-top: 0;
+  min-height: 300px;
+  max-height: 600px;
+}
+</style>
