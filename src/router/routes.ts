d6704e9d2c4b8e5c6380b1c4d01534e0f29f89c3
v-gabor <varadi.gabor@optimadesign.hu>
2020-05-20 08:39:25 +0200

    Define routes, #5

---
+++ src/router/routes.ts
@@ -0,0 +1,30 @@
+import { RouteConfig } from "vue-router";
+import Home from "../views/Home.vue";
+
+const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    redirect: { name: "Activity" },
+    children: [
+      {
+        name: "Activity",
+        path: "i-am-boring",
+        component: () =>
+          import(
+            /* webpackChunkName: "activity" */ "../components/Activity.vue"
+          ),
+      },
+      {
+        path: "my-activities",
+        component: () =>
+          import(
+            /* webpackChunkName: "my-activities" */ "../components/ActivityList.vue"
+          ),
+      },
+    ],
+  },
+];
+
+export default routes;
