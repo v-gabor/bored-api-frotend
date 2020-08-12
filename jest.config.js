79ea08f3a62d744609ac5cccd82cda95d13eae4f
v-gabor <varadi.gabor@optimadesign.hu>
2020-08-12 07:00:26 +0200

    Removing unnecessary rows

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   setupFilesAfterEnv: ["<rootDir>/tests/unit/setup.ts"],
-  // transform: { "^.*\\.js$": "babel-jest" },
-  // transformIgnorePatterns: ["node_modules/(?!vue-router|@babel|vuetify)"],
 };
